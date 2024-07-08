c7d86386d93c8cbcf961397b8d55a138847e3842
ohadApiiro <75303375+ohadApiiro@users.noreply.github.com>
2024-07-08 18:24:38 +0300

    Update Bar.py

--- Bar.py
+++ Bar.py
@@ -1,6 +1,6 @@
-# class Matoki:
-#     def func_in_matoki(self):
-#         pass
-#
-# def global_matoki():
-#     pass
+class Matoki:
+    def func_in_matoki(self):
+        pass
+
+def global_matoki():
+    pass
